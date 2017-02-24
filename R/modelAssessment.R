4bd09ca2fa259938081d8dafa0466cb29ea13d4e
Sara JC Gosline <sara.gosline@sagebase.org>
2017-02-23 18:14:19 -0800

    updated with LOO CV

--- R/modelAssessment.R
+++ R/modelAssessment.R
@@ -21,6 +21,9 @@ crossValidationCompare <- function(fendRObj,
   ##get a list of all samples
   all.samps<-intersect(fendRObj$sampleOutcomeData$Sample,fendRObj$featureData$Sample)
 
+  if(length(testPheno)==0)
+    testPheno <- unique(fendRObj$sampleOutcomeData$Phenotype)
+
   if(is.null(fendRObj$graph))
     fendRObj <- loadNetwork(fendRObj) ###only need to load graph once
 
@@ -32,35 +35,52 @@ crossValidationCompare <- function(fendRObj,
   origMatrix<-originalResponseMatrix(fendRObj)
   engMatrix<-engineeredResponseMatrix(fendRObj)
 
+
   #for each sample, leave one out
   vals<-lapply(all.samps,function(x){
 
+
     #subset out that data and re-assign original object
-    test.data<-subset(fendRObj$sampleOutcomeData,Sample==x)
+    test.df<-filter(fendRObj$sampleOutcomeData,Sample==x)%>%filter(Phenotype%in%testPheno)
+    test.data<-test.df$Response
+    names(test.data)<-test.df$Phenotype
     orig.test.features<-subset(fendRObj$featureData,Sample==x)
+    #artificially expand to do acast
     otf<-orig.test.features$Value
     names(otf)<-orig.test.features$Gene
 
-    aug.test.features<-subset(engMatrix,Sample==x)
-    atf<-aug.test.features$Value
-    names(atf)<-aug.test.features$Gene
+
+    aug.test.features<-subset(fendRObj$remappedFeatures,Sample==x)
+    atf<-reshape2::acast(select(aug.test.features,Phenotype,Gene,Value),Phenotype~Gene)
+
 
     #build original and updated model
-    orig.mod<-do.call(modelCall,list(formula='Response~.',data=origMatrixList[[p]]))
-    orig.pred<-predict(orig.mod,data=otf)
+    orig.pred<-sapply(testPheno,function(p){
+      mod.dat<-filter(origMatrix,Sample!=x)%>%filter(Phenotype==p)%>%select(-Phenotype,-Sample)
+ #   zvars<-which(apply(mod.dat,2,var)==0)
+#    if(length(zvars)>0)
+#      mod.dat<-mod.dat[,-zvars]
+      orig.mod<-do.call(modelCall,list(formula='Response~.',data=mod.dat))
+      predict(orig.mod,newdata=data.frame(t(otf)))[[1]]
+    })
 
-    eng.mod<-do.call(modelCall,list(formula='Response~.',data=engMatrixList[[p]]))
-    eng.pred<-predict(eng.mod,data=atf)
+    mod.dat<-filter(engMatrix,Sample!=x)%>%select(-Phenotype,-Sample)
+ #   zvars<-which(apply(mod.dat,2,var)==0)
+#    if(length(zvars)>0)
+#      mod.dat<-mod.dat[,-zvars]
+    eng.mod<-do.call(modelCall,list(formula='Response~.',data=mod.dat))
+    eng.pred<-predict(eng.mod,data.frame(atf))
 
 
-      df<-data.frame(OriginalPred=orig.pred,EngineeredPred=eng.pred)
-      df$Pheno=rep(p,nrow(df))
-      df$LeftOutSample=rep(x,nrow(df))
-      df$Vals<-test.data
+      df<-data.frame(OriginalPred=orig.pred,
+          EngineeredPred=eng.pred,
+          Sample=rep(x,length(testPheno)),TrueValue=test.data)
+
       df
       })
-    all.res<-do.call('rbind',vals)
 
+    all.res<-do.call('rbind',vals)
+  vals
 
 
 }
